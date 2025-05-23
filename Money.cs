b5fd895479b79d79e20c8cfc75619eefc014eabe
KatyaRa17 <sosninae17@gmail.com>
2025-05-23 14:45:30 +0500

    Add files via upload

---
+++ Money.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace лаба2_23 {
+  public class Money {
+    private uint rubles;
+    private byte kopeks;
+
+    public uint Rubles {
+      get { return rubles; }
+      private set { rubles = value; }
+    }
+
+    public byte Kopeks {
+      get { return kopeks; }
+      private set { kopeks = value; }
+    }
+
+    public Money(uint rubles, byte kopeks) {
+      if (kopeks >= 100) {
+        throw new ArgumentException("Копейки должны быть меньше 100");
+      }
+      this.rubles = rubles;
+      this.kopeks = kopeks;
+    }
+
+    public override string ToString() {
+      return $"{rubles} руб. {kopeks} коп.";
+    }
+
+    public Money AddKopeks(uint additionalKopeks) {
+      uint totalKopeks = kopeks + additionalKopeks;
+      uint newRubles = rubles + (totalKopeks / 100);
+      byte newKopeks = (byte)(totalKopeks % 100);
+
+      return new Money(newRubles, newKopeks);
+    }
+
+    public static Money operator ++(Money money) {
+      return money.AddKopeks(1);
+    }
+
+    public static Money operator --(Money money) {
+      if (money.Kopeks == 0 && money.Rubles == 0) {
+        throw new InvalidOperationException("Нельзя вычесть копейку из 0.");
+      }
+
+      uint totalKopeks = (money.Rubles * 100) + money.Kopeks - 1;
+      uint newRubles = totalKopeks / 100;
+      byte newKopeks = (byte)(totalKopeks % 100);
+
+      return new Money(newRubles, newKopeks);
+    }
+
+    public static explicit operator uint(Money money) {
+      return money.Rubles;
+    }
+
+    public static implicit operator double(Money money) {
+      return ((double)money.Kopeks / 100) + money.Rubles;
+    }
+
+    public static Money operator +(Money money, uint value) {
+      return money.AddKopeks(value);
+    }
+
+    public static Money operator +(uint value, Money money) {
+      return money + value;
+    }
+
+    public static Money operator -(Money money, uint value) {
+      uint totalKopeks = (money.Rubles * 100) + money.Kopeks;
+      if (value > totalKopeks) {
+        throw new InvalidOperationException("Результат вычитания не может быть отрицательным.");
+      }
+
+      totalKopeks -= value;
+      uint newRubles = totalKopeks / 100;
+      byte newKopeks = (byte)(totalKopeks % 100);
+
+      return new Money(newRubles, newKopeks);
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,32 +1,56 @@
 ﻿/* продолжение задания №23 */
 
 using System;
-using System.Linq;
 
-namespace ConsoleApp3 {
+namespace лаба2_23 {
   public class Program {
-    public static void Main() {
-      // Тестирование базового класса
-      Console.WriteLine("Введите любой текст:");
-      Base baseObject = new Base(Console.ReadLine());
-      baseObject.AddExclamations();
-      Console.WriteLine(baseObject.ToString());
+    public static void Main(string[] args) {
+      try {
+        Console.WriteLine("Введите сумму в рублях:");
+        uint inputRubles = uint.Parse(Console.ReadLine());
+        Console.WriteLine("Введите сумму в копейках:");
+        byte inputKopeks = byte.Parse(Console.ReadLine());
 
-      // Тестирование конструктора копирования базового класса
-      Base baseCopy = new Base(baseObject);
-      Console.WriteLine(baseCopy.ToString());
+        Money moneyObject = new Money(inputRubles, inputKopeks);
+        Console.WriteLine("Создан объект Money: " + moneyObject.ToString());
 
-      // Тестирование дочернего класса
-      Derived derivedObject = new Derived("Дочерний класс", 17, DateTime.Now);
-      Console.WriteLine(derivedObject.DisplayInfo());
+        Console.WriteLine("Введите количество копеек для добавления:");
+        uint additionalKopeks = uint.Parse(Console.ReadLine());
 
-      // Тест конструктора копирования дочернего класса
-      Derived derivedCopy = new Derived(derivedObject);
-      Console.WriteLine(derivedCopy.DisplayInfo());
+        Money updatedMoney = moneyObject.AddKopeks(additionalKopeks);
+        Console.WriteLine("Новая сумма: " + updatedMoney.ToString());
 
-      // Изменение числа в дочернем классе
-      derivedCopy.UpdateNumber(100);
-      Console.WriteLine(derivedCopy.DisplayInfo());
+        Console.WriteLine("Тест операции ++: " + (++updatedMoney).ToString());
+        Console.WriteLine("Тест операции --: " + (--updatedMoney).ToString());
+
+        uint moneyAsUint = (uint)updatedMoney;
+        Console.WriteLine("Приведение к uint: " + moneyAsUint);
+
+        double moneyAsDouble = updatedMoney;
+        Console.WriteLine("Приведение к double: " + moneyAsDouble);
+
+        Console.WriteLine("Введите число для сложения:");
+        uint valueToAdd = uint.Parse(Console.ReadLine());
+        Money sum = updatedMoney + valueToAdd;
+        Console.WriteLine("Сумма: " + sum.ToString());
+
+        Money sumCommutative = valueToAdd + updatedMoney;
+        Console.WriteLine("Сумма (коммут.): " + sumCommutative.ToString());
+
+        Console.WriteLine("Введите число для вычитания:");
+        uint valueToSubtract = uint.Parse(Console.ReadLine());
+        Money diff = updatedMoney - valueToSubtract;
+        Console.WriteLine("Разность: " + diff.ToString());
+      }
+      catch (FormatException) {
+        Console.WriteLine("Ошибка: неверный формат ввода!");
+      }
+      catch (ArgumentException ex) {
+        Console.WriteLine("Ошибка: " + ex.Message);
+      }
+      catch (InvalidOperationException ex) {
+        Console.WriteLine("Ошибка: " + ex.Message);
+      }
     }
   }
 }
